418cd01a5d553643bc7d57dff7f50020686856c7
Eben-Success <aebenezer237@gmail.com>
2023-02-19 00:55:58 +0000

    update

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const dbConnect = require('./config/dbconnect');
 const app = express()
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute");
+const bodyParser = require('body-parser');
 
 dbConnect();
 
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res) => {
-    res.send("Hello from server side")
-})
+
+
+// Routes
+app.use("/api/user", authRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);

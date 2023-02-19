38484dcd887fd71f689112477ac2df684661887e
Eben-Success <aebenezer237@gmail.com>
2023-02-19 00:20:41 +0000

    update

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,5 +1,17 @@
 const { default: mongoose } = require("mongoose")
+const dotenv = require("dotenv").config();
+
+mongoose.set("strictQuery", true)
 
 const dbConnect = () => {
-    const connect = mongoose.connect()
-}
+    try{
+        const connect = mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected successfully");
+    }
+    catch(error){
+        console.log("Database connection failed");
+        }
+
+    }
+
+module.exports = dbConnect
